9912cb4dbc049760dbe3f4df6b294dc5489b3bc6
tkachuk-labs <tkachuk.labs@gmail.com>
2021-11-29 14:01:43 +0200

    README.md and release-publish-all.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -6,4 +6,4 @@ BASE_IMAGE="$1"
 PLATFORM="$2"
 NIX_VERSION="$3"
 
-docker buildx build -t "heathmont/nix-$BASE_IMAGE-$PLATFORM:$NIX_VERSION" -f "./Dockerfile-nix-$BASE_IMAGE:$NIX_VERSION" --platform "linux/$PLATFORM" .
+docker buildx build -t "heathmont/nix:$BASE_IMAGE-$PLATFORM-$NIX_VERSION" -f "./Dockerfile-nix-$BASE_IMAGE:$NIX_VERSION" --platform "linux/$PLATFORM" .
