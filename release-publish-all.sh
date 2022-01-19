85ec3801212859c5b9c6553f08d959d548624eb6
tkachuk-labs <tkachuk.labs@gmail.com>
2022-01-19 23:30:41 +0200

    21it

--- release-publish-all.sh
+++ release-publish-all.sh
@@ -6,7 +6,7 @@ for BASE_IMAGE in alpine ubuntu; do
   for PLATFORM in arm64 x86_64; do
     for NIX_VERSION in "2.3.15"; do
       ./release.sh "$BASE_IMAGE" "$PLATFORM" "$NIX_VERSION"
-      docker push "heathmont/nix:$BASE_IMAGE-$PLATFORM-$NIX_VERSION"
+      docker push "21it/nix:$BASE_IMAGE-$PLATFORM-$NIX_VERSION"
     done
   done
 done
